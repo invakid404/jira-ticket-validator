deadf4f4c62f974435a06fa51af671b0576de2c9
invakid404 <invakid404@riseup.net>
2021-08-20 22:29:11 +0300

    feat: initialize jira client and fetch ticket

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import * as core from '@actions/core';
 
+import { jira } from './jira';
+
 (async (): Promise<void> => {
-  core.info('Hello, world!');
+  const ticket = core.getInput('ticket', { required: true });
+
+  try {
+    const ticketData = await jira.findIssue(ticket);
+
+    core.info(JSON.stringify(ticketData, null, 2));
+  } catch (error) {
+    core.setFailed(`"${ticket}" is not a valid Jira ticket!`);
+  }
 })();
